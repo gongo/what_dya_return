a3b70cd5ea4c58bba6e21904f75a680dfe03a372
Wataru MIYAGUNI <gonngo@gmail.com>
2023-05-04 09:43:10 +0900

    :tada: Start
    ```
    bundle gem what_dya_return --no-exe --mit --test=test-unit --ci=github --linter=standard
    ```

---
+++ lib/what_dya_return.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "what_dya_return/version"
+
+module WhatDyaReturn
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "what_dya_return"
+
+require "test-unit"
