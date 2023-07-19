e216c79a45129652317155a0ce7f0c67a21c2bc4
Wataru MIYAGUNI <gonngo@gmail.com>
2023-07-20 00:16:26 +0900

    :white_check_mark: Refactor test cases.

--- test/what_dya_return/extractor_test.rb
+++ test/what_dya_return/extractor_test.rb
@@ -41,7 +41,7 @@ module WhatDyaReturn
     end
 
     def test_return_integer_using_return_statement
-      assert_extract_values(<<-CODE, ['42'])
+      assert_extract_values(<<-CODE, %w[42])
         def foo
           return 42
         end
@@ -67,7 +67,7 @@ module WhatDyaReturn
     end
 
     def test_conditinal_with_if_modifier_must_true
-      assert_extract_values(<<-CODE, ['42'])
+      assert_extract_values(<<-CODE, %w[42])
         def foo
           42 if true
         end
@@ -83,107 +83,107 @@ module WhatDyaReturn
     end
 
     def test_conditional_with_if_else
-      assert_extract_values(<<-CODE, ['42', '"baz"'])
+      assert_extract_values(<<-CODE, %w[1 2])
         def foo
           if bar
-            42
+            1
           else
-            'baz'
+            2
           end
         end
       CODE
     end
 
     def test_conditional_with_if_elsif_else
-      assert_extract_values(<<-CODE, ['42', '"baz"', ':piyo'])
+      assert_extract_values(<<-CODE, %w[1 2 3])
         def foo
           if bar
-            42
+            1
           elsif baz
-            'baz'
+            2
           else
-            :piyo
+            3
           end
         end
       CODE
     end
 
     def test_conditional_with_if_else_only_if_branch
-      assert_extract_values(<<-CODE, ['42'])
+      assert_extract_values(<<-CODE, %w[1])
         def foo
           if true
-            42
+            1
           else
-            'baz'
+            2
           end
         end
       CODE
     end
 
     def test_conditional_with_if_else_only_else_branch
-      assert_extract_values(<<-CODE, ['"baz"'])
+      assert_extract_values(<<-CODE, %w[2])
         def foo
           if false
-            42
+            1
           else
-            'baz'
+            2
           end
         end
       CODE
     end
 
     def test_conditional_with_unless_else_only_unless_branch
-      assert_extract_values(<<-CODE, ['42'])
+      assert_extract_values(<<-CODE, %w[1])
         def foo
           unless false
-            42
+            1
           else
-            'baz'
+            2
           end
         end
       CODE
     end
 
     def test_conditional_with_unless_else_only_else_branch
-      assert_extract_values(<<-CODE, ['"baz"'])
+      assert_extract_values(<<-CODE, %w[2])
         def foo
           unless true
-            42
+            1
           else
-            'baz'
+            2
           end
         end
       CODE
     end
 
     def test_conditional_with_if_elsif_else_exclude_elsif
-      assert_extract_values(<<-CODE, ['42', ':piyo'])
+      assert_extract_values(<<-CODE, %w[1 3])
         def foo
           if bar
-            42
+            1
           elsif false
-            'baz'
+            2
           else
-            :piyo
+            3
           end
         end
       CODE
     end
 
     def test_conditional_with_nested_if_else
-      assert_extract_values(<<-CODE, ['42', ':piyo', '"quux"', '"corge"'])
+      assert_extract_values(<<-CODE, %w[1 2 3 4])
         def foo
           if bar
             if baz
-              42
+              1
             else
-              :piyo
+              2
             end
           else
             if qux
-              'quux'
+              3
             else
-              'corge'
+              4
             end
           end
         end
@@ -191,31 +191,31 @@ module WhatDyaReturn
     end
 
     def test_conditinal_with_ternary
-      assert_extract_values(<<-CODE, ['42', '"baz"'])
+      assert_extract_values(<<-CODE, %w[1 2])
         def foo
-          bar ? 42 : 'baz'
+          bar ? 1 : 2
         end
       CODE
     end
 
     def test_conditinal_with_ternary_only_former
-      assert_extract_values(<<-CODE, ['42'])
+      assert_extract_values(<<-CODE, %w[1])
         def foo
-          true ? 42 : 'baz'
+          true ? 1 : 2
         end
       CODE
     end
 
     def test_conditinal_with_ternary_only_latter
-      assert_extract_values(<<-CODE, ['"baz"'])
+      assert_extract_values(<<-CODE, %w[2])
         def foo
-          false ? 42 : 'baz'
+          false ? 1 : 2
         end
       CODE
     end
 
     def test_multiline
-      assert_extract_values(<<-CODE, ['3'])
+      assert_extract_values(<<-CODE, %w[3])
         def foo
           1
           2
@@ -245,47 +245,47 @@ module WhatDyaReturn
     end
 
     def test_conditional_with_case_when
-      assert_extract_values(<<-CODE, ['"1"', '"2"', ''])
+      assert_extract_values(<<-CODE, ['1', '2', ''])
         def foo
           case bar
-          when 1
-            '1'
-          when 2
-            '2'
+          when '1'
+            1
+          when '2'
+            2
           end
         end
       CODE
     end
 
     def test_conditional_with_case_when_else
-      assert_extract_values(<<-CODE, ['"1"', '"2"', '"3"', '"4"'])
+      assert_extract_values(<<-CODE, %w[1 2 3 4])
         def foo
           case bar
           when false
-            '1'
+            1
           when 2
-            '2'
+            2
           when nil
-            '3'
+            3
           else
-            '4'
+            4
           end
         end
       CODE
     end
 
     def test_conditional_with_case_when_else_no_case_condition
-      assert_extract_values(<<-CODE, ['"2"', '"4"'])
+      assert_extract_values(<<-CODE, %w[2 4])
         def foo
           case
           when false
-            '1'
+            1
           when 2
-            '2'
+            2
           when nil
-            '3'
+            3
           else
-            '4'
+            4
           end
         end
       CODE
